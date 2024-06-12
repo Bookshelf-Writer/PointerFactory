ea41d5a2a896d907b8008a938743c1e08af7acab
SunSung W541 <mail@embedded.biz.ua>
2024-06-12 18:09:59 +0200

    fix [1.0.3]  add const

--- const.go
+++ const.go
@@ -3,10 +3,13 @@ package PointerFactory
 const (
 	GlobalName       string = "PointerFactory"
 	GlobalDateUpdate string = "06-12-2024"
-	GlobalHash       string = "ce3d3973d10794da1f256ba98e4f2792bfe228a4"
+	GlobalHash       string = "4881e244bd6fe2b7ed5d9cf86111fb108a2f098c"
 
-	GlobalVersion      string = "1.0.1"
+	GlobalVersion      string = "1.0.3"
 	GlobalVersionMajor string = "1"
 	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 1
+	GlobalVersionPatch uint16 = 3
+
+	NumBase   = 36
+	YearPoint = 2024
 )
