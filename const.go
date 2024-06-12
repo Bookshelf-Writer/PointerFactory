4f5b194fc0d0716e31d1e76693dc385d12f4a9bb
SunSung W541 <mail@embedded.biz.ua>
2024-06-12 18:14:17 +0200

    main [v1.0.4]  go version

--- const.go
+++ const.go
@@ -3,12 +3,12 @@ package PointerFactory
 const (
 	GlobalName       string = "PointerFactory"
 	GlobalDateUpdate string = "06-12-2024"
-	GlobalHash       string = "4881e244bd6fe2b7ed5d9cf86111fb108a2f098c"
+	GlobalHash       string = "b64913809bdf77ac1d2f6af6044a7e5c1dbc0d3a"
 
-	GlobalVersion      string = "1.0.3"
-	GlobalVersionMajor string = "1"
+	GlobalVersion      string = "v1.0.4"
+	GlobalVersionMajor string = "v1"
 	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 3
+	GlobalVersionPatch uint16 = 4
 
 	NumBase   = 36
 	YearPoint = 2024
