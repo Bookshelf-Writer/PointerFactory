a213d9905762e148733bebbaf5dc251e92f5e3c0
GitHub Action <action@github.com>
2024-06-12 16:15:30 +0000

    actions [v1.1.0] Build: [v1.0.4] >> [v1.1.0]

--- const.go
+++ const.go
@@ -1,15 +1,15 @@
 package PointerFactory
 
 const (
-	GlobalName       string = "PointerFactory"
-	GlobalDateUpdate string = "06-12-2024"
-	GlobalHash       string = "b64913809bdf77ac1d2f6af6044a7e5c1dbc0d3a"
+	 GlobalName string = "PointerFactory"
+	 GlobalDateUpdate string = "06-12-2024"
+	 GlobalHash string = "4f5b194fc0d0716e31d1e76693dc385d12f4a9bb"
 
-	GlobalVersion      string = "v1.0.4"
-	GlobalVersionMajor string = "v1"
-	GlobalVersionMinor uint16 = 0
-	GlobalVersionPatch uint16 = 4
+	 GlobalVersion string = "v1.1.0"
+	 GlobalVersionMajor string = "v1"
+	 GlobalVersionMinor uint16 = 1
+	 GlobalVersionPatch uint16 = 0
 
-	NumBase   = 36
-	YearPoint = 2024
+	 NumBase = 36
+	 YearPoint = 2024
 )
